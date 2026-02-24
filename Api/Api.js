e44a2ccbc4ae33ee0389cf7d79f2d586be0c5230
Felipe Alvariño <felipeignacioal170@gmail.com>
2026-02-24 12:08:47 -0300

    Create Api.js

---
+++ Api/Api.js
@@ -0,0 +1,46 @@
+const URL = "https://api-react-taller-production.up.railway.app";
+
+// =====================
+// REGISTER
+// =====================
+export const register = async (username, name, password) => {
+  const res = await fetch(`${URL}/api/auth/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, name, password }),
+  });
+
+  return res.json();
+};
+
+// =====================
+// LOGIN
+// =====================
+export const login = async (username, password) => {
+  const res = await fetch(`${URL}/api/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+
+  return res.json();
+};
+
+// =====================
+// PRODUCTOS (TEMPORAL)
+// =====================
+export async function obtenerProductos() {
+  const res = await fetch("https://fakestoreapi.com/products");
+
+  if (!res.ok) throw new Error("Error cargando productos");
+
+  return res.json();
+}
+
+export async function obtenerProducto(id) {
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+
+  if (!res.ok) throw new Error("Error cargando producto");
+
+  return res.json();
+}
